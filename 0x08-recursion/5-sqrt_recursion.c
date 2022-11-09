0b7b09926d75bbaafd2fae566bf6417904307e2e
Israel737 <layiwolaolamide737@gmail.com>
2022-11-09 12:02:52 -0800

    task 6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,35 @@
 #include "main.h"
 
-/**
- * is_prime_number - returns the 1 if n is prime
- * @n: number to be checked
- *
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime_number(int n)
-{
-	int start = n / 2;
+int sqrt_func(int n, int i);
+int _sqrt_recursion(int n);
 
-	if (n <= 1)
-		return (0);
-	return (is_prime(n, start));
+/**
+ * _sqrt_recursion - a function that returns the natural square root of
+ * a number
+ * @n: the natural number to be evaluated
+ * Return: returns -1 if n does not have a natural squareroot else, it
+ * returns the squareroot of the natural number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (sqrt_func(n, 0));
 }
 
 /**
- * is_prime - returns the 1 if n is prime
- * @n: number to be checked
- * @start: number to start checking from
- *
- * Return: 1 if n is prime, 0 otherwise
+ * sqrt_func - a function that solves for the squareroot of a number
+ * @n: the natural number to be evaluated
+ * @i: iteretor (test case)
+ * Return:  the squareroot of the natural number
  */
-int is_prime(int n, int start)
+int sqrt_func(int n, int i)
 {
-	if (start <= 1)
-		return (1);
-	else if (n % start == 0)
-		return (0);
-	return (is_prime(n, start - 1));
+	if ((i * i) > n)
+		return (-1);
+	if ((i * i) == n)
+		return (i);
+	else
+		return (sqrt_func(n, ++i));
 }
