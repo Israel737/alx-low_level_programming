e8e93dfdb949ae3b34ec9ffc743cac1f52871022
Israel737 <layiwolaolamide737@gmail.com>
2022-11-13 05:32:13 -0800

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - a program that prints teh number of arguments passed into it
+ * @argc: the number of arguments passed during runtime of the program
+ * @argv: an array of strings comtaining the strings passed during runtime
+ * Return: returns always
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]);
+int _putchar(char c);
+
+
+#endif
